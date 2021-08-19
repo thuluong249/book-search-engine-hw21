6ea7364d2514d14b7e5c42c8eba83af2e4ec60d5
Thu Luong <thuluong@Thus-MacBook-Pro.local>
2021-08-18 22:09:14 -0400

    push up to gh first time

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # hw21-book-search-engine
+
+[![License: MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+![bannerImage](./Assets/banner.png)
+
+
+# 📋 Description 📋
+
+```
+This is a full stack MERN application for searching Books API with React front end, MongoDB database, Node.js/Express.js server and API.
+```
+
+# 💼 Technologies Used 💼
+
+* `Node.js`
+* `npm (Node Package)`
+* `Javascript`
+* `React`
+* `React Router`
+* `CSS`
+
+
+# 📸 Screenshots 📸
+
+![Home](./Assets/homepage.png)
+
+
+<!-- ![About](./Assets/about.png)
+
+
+![Projects](./Assets/projects.png)
+
+
+![About](./Assets/contact.png)
+ -->
+
+
+# 🔗 Application Links
+
+* [Github](https://thuluong249.github.io/hw21-book-search-engine/)
+
+# 📱 Contact 
+
+* [Github](https://github.com/thuluong249)
+
+* <a href="mailto:thujtn2019@gmmail.com">💌 Email</a> 
+
+---
---
+++ client/src/utils/mutation.js
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+	mutation loginUser($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				email
+				username
+			}
+		}
+	}
+`;
+
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const SAVE_BOOK = gql`
+	mutation saveBook($bookBody: BookInput!) {
+		saveBook(bookBody: $bookBody) {
+			_id
+			username
+			bookCount
+			savedBooks {
+				bookId
+				authors
+				title
+				description
+				image
+				link
+			}
+		}
+	}
+`;
+
+export const REMOVE_BOOK = gql`
+	mutation removeBook($bookId: String!) {
+		removeBook(bookId: $bookId) {
+			_id
+			username
+			bookCount
+			savedBooks {
+				bookId
+				authors
+				title
+				description
+				image
+				link
+			}
+		}
+	}
+`;
