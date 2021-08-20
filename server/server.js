3805db450e0336d33f9c8fb8f4ec0432808cbdcf
Thu Luong <thuluong@Thus-MacBook-Pro.local>
2021-08-19 20:38:07 -0400

    added graphql to the server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,7 +20,7 @@ async function startServer() {
 	server.applyMiddleware({ app });
 }
 
-startServer;
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
